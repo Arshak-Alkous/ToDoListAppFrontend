2bdf70a67095cf6a7b3fa5bc1c41c12c39bf4638
Arshak <arshak.alkous@hotmail.com>
2025-01-24 11:58:15 +0100

    modify a printProjectToTable and printTaskTotable alittle bit

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 
 function printProjectsToTable(){
-    let projectTable = createTable("ProjectId","ProjectName","ProjectTable");
+    let projectTable = createTable2("ProjectId","ProjectName","status","projectTags","ProjectTable");
     fetch("https://localhost:7028/Project")
     .then(res=> res.json())
     .then(project=> project.forEach(element => {
         let row = document.createElement("tr");
-        elementCreator("td",element.id,row);
+        elementCreator("td",element.projectId,row);
         elementCreator("td",element.name,row);
+        let status =element.isActive===true?"Active":"Not Active";
+        elementCreator("td",status,row);
+        let projectTags=[];
+        element.tags.forEach(t=>projectTags.push(t.name));
+        elementCreator("td",projectTags,row);
         projectTable.appendChild(row);
     }));
 }
 function printTasksToTable(){
-    let taskTable = createTable("TaskId","TaskName","TaskTable");
+    let taskTable = createTable2("TaskId","TaskName","Deadline","projectId","TaskTable");
     fetch("https://localhost:7028/Task")
     .then(res=> res.json())
     .then(task=> task.forEach(element => {
         let row = document.createElement("tr");
-        elementCreator("td",element.id,row);
+        elementCreator("td",element.taskId,row);
         elementCreator("td",element.name,row);
+        elementCreator("td",element.deadline,row);
+        elementCreator("td",element.projectId,row);
         taskTable.appendChild(row);
     }));
 }
@@ -27,7 +34,7 @@ function printTagsToTable(){
     .then(res=> res.json())
     .then(tag=> tag.forEach(element => {
         let row = document.createElement("tr");
-        elementCreator("td",element.id,row);
+        elementCreator("td",element.tagId,row);
         elementCreator("td",element.name,row);
         tagTable.appendChild(row);
     }));
@@ -45,10 +52,23 @@ function elementCreator(elementType, nodecontent, target){
     let target = document.getElementById(divId);
     let elementTable = document.createElement("table");
     target.appendChild(elementTable);
-    let tableRow = document.createElement("tr");
-    elementCreator("th",th1,tableRow);
-    elementCreator("th",th2,tableRow);
-    elementTable.appendChild(tableRow);
+    let tableRowHead = document.createElement("tr");
+    elementCreator("th",th1,tableRowHead);
+    elementCreator("th",th2,tableRowHead);
+    elementTable.appendChild(tableRowHead);
+    return elementTable;
+
+ }
+ function createTable2(th1,th2,th3,th4,divId){
+    let target = document.getElementById(divId);
+    let elementTable = document.createElement("table");
+    target.appendChild(elementTable);
+    let tableRowHead = document.createElement("tr");
+    elementCreator("th",th1,tableRowHead);
+    elementCreator("th",th2,tableRowHead);
+    elementCreator("th",th3,tableRowHead);
+    elementCreator("th",th4,tableRowHead);
+    elementTable.appendChild(tableRowHead);
     return elementTable;
 
  }
