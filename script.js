9969a8ebfd8d1a6c2dd864759e7cd16531ae043e
Arshak <arshak.alkous@hotmail.com>
2025-02-07 11:43:30 +0100

    biuld first html page with css file and I added toggleMenu function to show all projects and all tags and editTag function to update certain tag and showTab to be able to switch between tabs

--- script.js
+++ script.js
@@ -1,4 +1,80 @@
-
+function toggleMenu(id) {
+    var submenu = document.getElementById(id);
+    var arrow = submenu.previousElementSibling.querySelector("span");
+    if (submenu.style.display === "block") {
+        submenu.style.display = "none";
+        arrow.textContent = "▶";
+    } else {
+        submenu.style.display = "block";
+        arrow.textContent = "▼";
+        if (id ==="projects"&& !submenu.dataset.loaded){
+            fetch("https://localhost:7028/Project")
+                .then(res => res.json())
+                .then(projects => {
+                    let target=document.getElementById("projects");
+                    target.innerHTML="";
+                    projects.forEach(project =>{
+                        elementCreator("p",project.name,target);
+                    });
+                    submenu.dataset.loaded= "true";
+                })
+                .catch(error=> console.error("faild to fetech perojects",error));
+        }
+        if (id=== "tags" && !submenu.dataset.loaded){
+            fetch("https://localhost:7028/Tag")
+                .then(res=> res.json())
+                .then(tags=> {
+                    let target = document.getElementById("tags");
+                    //let btnTarget= document.getElementById("menu-tags")
+                    target.innerHTML="";
+                    //let addButton = elementCreator("button","+",arrow);
+                    //addButton.classList.add("add-button");
+                    tags.forEach(tag =>{
+                        let tagElement= elementCreator("p",tag.name, target);
+                        let editButton= elementCreator("button","✏️",tagElement);
+                        let deletButton= elementCreator("button","❌",tagElement);
+                        editButton.onclick= ()=> editTag(tag.tagId);
+                        deletButton.onclick = ()=> deleteTag(tag.tagId);
+                    });
+                    submenu.dataset.loaded= "true";
+                })
+                .catch(error=> console.error("faild to fetch tags",error));
+        }
+       
+    }
+}
+function editTag(id){
+    alert("update tag th Id: "+id);
+    let newName= prompt("Enter new tag name:");
+    console.log(newName);
+    if(newName){
+        const updateTag={name:newName};
+        fetch(`https://localhost:7028/Tag/${id}`,{
+            method: "PUT",
+            headers:{"Content-Type":"application/json"},
+            body : JSON.stringify(updateTag),
+        })
+        .then (()=> console.log("update tag successfully"))
+        .then(()=> location.reload())
+        .catch(error=> console.error("error is:",error ));
+    }
+}
+function deleteTag(id){
+    alert("Delete tag with Id: "+id);
+}
+function showTab(tabId) {
+    var contents = document.querySelectorAll(".tab-content");
+    contents.forEach(content => content.classList.remove("active-tab"));
+    document.getElementById(tabId).classList.add("active-tab");
+}
+function printProjectsToDropdown(){
+    fetch("https://localhost:7028/Project")
+    .then(res => res.json())
+    .then(project => project.forEach(element =>{
+        let target=document.getElementById("projects");
+        elementCreator("p",element.name,target);
+    }));
+}
 function printProjectsToTable(){
     let projectTable = createTable2("ProjectId","ProjectName","status","projectTags","ProjectTable");
     fetch("https://localhost:7028/Project")
